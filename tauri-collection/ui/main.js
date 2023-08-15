0c821f41bf23b07d3418885bcbf446c84f870094
Thomas Stegmaier <thosteg@googlemail.com>
2023-08-15 16:49:40 +0200

    Single Button for js and rust callback, long callback

--- tauri-collection/ui/main.js
+++ tauri-collection/ui/main.js
@@ -1,16 +1,71 @@
+/**
+ * Content adapted from default javascript created create-tauti-app
+ */
+
 const { invoke } = window.__TAURI__.tauri
 
-let greetInputEl;
-let greetMsgEl;
+let greetInput;
+let greetMsgJs;
+let greetMsgRs;
 
-async function greet() {
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+function init_vars() {
+  greetInput = document.querySelector("#greet-input");
+  greetMsgJs = document.querySelector("#greet-msg-js");
+  greetMsgRs = document.querySelector("#greet-msg-rs");
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
+function init_event_functions() {
   document
     .querySelector("#greet-button")
     .addEventListener("click", () => greet());
+  document
+    .querySelector("#long-callback-button")
+    .addEventListener("click", () => long_response());
+}
+
+/* Status */
+
+function status_message(message) {
+  let s = document.querySelector("#statusbar");
+  s.textContent = message;
+}
+
+/* Simple manipulation */
+
+function greet() {
+  setTimeout(function() {greet_rs(); }, 0);
+  setTimeout(function() {greet_js(); }, 0);
+}
+
+function greet_js() {
+  greetMsgJs.textContent =  "Sei gegrüßt, " + greetInput.value + "!";
+}
+
+/* IPC */
+
+async function greet_rs() {
+  greetMsgRs.textContent = await invoke("greet", { name: greetInput.value });
+}
+
+/* Repetitive */
+
+function long_response() {
+  let el = document.querySelector("#long-duration-callback");
+  if (el) {
+    status_message("adding selected");
+
+    el.classList.add("selected");
+    window.setTimeout(function() {
+      status_message("removing selected");
+
+      el.classList.remove("selected");
+    }, 600);
+  }
+}
+
+/* Mother */
+
+window.addEventListener("DOMContentLoaded", () => {
+  init_vars();
+  init_event_functions();
 });
