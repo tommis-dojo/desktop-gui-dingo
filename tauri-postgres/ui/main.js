bb77da70f0a24c5b256ea594d446d405b054ab3b
Thomas Stegmaier <thosteg@googlemail.com>
2024-06-14 08:33:31 +0200

    Breadcrumbs work

--- tauri-postgres/ui/main.js
+++ tauri-postgres/ui/main.js
@@ -5,9 +5,11 @@
 const { invoke } = window.__TAURI__.tauri
 
 let dbTable;
+let breadcrumbs;
 
 function initVariables() {
   dbTable = document.querySelector("#db-table table");
+  breadcrumbs = document.querySelector("#breadcrumbs");
 }
 
 function initEventFunctions() {
@@ -17,6 +19,62 @@ function initEventFunctions() {
 }
 
 /* Status */
+
+function db_path_shell() {
+  return {
+    "connection_string": null,
+    "database": null,
+    "table": null
+  };
+}
+
+function addTextLiTo(parent, className, textContent, linkFunction) {
+  let li = document.createElement('li');
+    
+  li.appendChild(document.createTextNode(textContent));
+
+  if (linkFunction !== null) {
+    li.addEventListener("click", linkFunction);
+  }
+
+  if (className !== null) {
+    li.className = className;
+  }
+  
+  parent.appendChild(li);  
+}
+
+// Add an element to the breadcrumbs including link
+function addBreadcrumbIfGiven(path, altLinkText, pathElements) {
+  let subPath = db_path_shell();
+  pathElements.forEach(key => { subPath[key] = path[key];});
+
+  let key = pathElements[pathElements.length - 1];
+  let lastPathElement = path[key];
+
+  if (lastPathElement !== null) {
+    addTextLiTo(breadcrumbs, "breadcrumb_separator", ">", null);
+
+    // Choose Text for link
+    let linkText;
+    if (altLinkText !== null) {
+      linkText = altLinkText;
+    } else {
+      linkText = lastPathElement
+    }
+
+    addTextLiTo(breadcrumbs, "link", linkText, () => dbRequest(subPath));
+  }
+}
+
+function updateBreadcrumbs(path) {
+  // Breadcrumbs show everything but the main information
+  breadcrumbs.innerHTML = '';
+
+  addBreadcrumbIfGiven(path, "home", ["connection_string"]);
+  addBreadcrumbIfGiven(path, null, ["connection_string","database"]);
+  addBreadcrumbIfGiven(path, null, ["connection_string","database","table"]);
+}
 
 function updateDatabasePath(path) {
   document.querySelector("#databasePath .connection_string").value = path["connection_string"];
@@ -64,11 +122,12 @@ function replaceTableContents(rows) {
   }
 }
 
-async function dbRequest(params) {
-  // Hint: just replace query
-  invoke("db_query",params)
+async function dbRequest(dbPath) {
+  InformStatus("Note dbRequest: " + JSON.stringify(dbPath))
+  invoke("db_query",{ query: dbPath})
     .then((message) => {
       replaceTableContents(message);
+      updateBreadcrumbs(dbPath);  // just copy the query verbatim as current path
     })
     .catch((error) => InformStatus("Error: " + error));
 }
@@ -88,21 +147,25 @@ function valueFromPath(selectors) {
   return value;
 }
 
+/**
+ * 
+ */
 async function dbRequestFromDbPath() {
-  let params = {};
-  params["connection_string"] = valueFromPath("#databasePath .connection_string");
-  params["database"] = valueFromPath("#databasePath .database");
-  params["table"] = valueFromPath("#databasePath .table");
-  await dbRequest({ query: params});
+  let dbPath = {};
+  dbPath["connection_string"] = valueFromPath("#databasePath .connection_string");
+  dbPath["database"] = valueFromPath("#databasePath .database");
+  dbPath["table"] = valueFromPath("#databasePath .table");
+  await dbRequest(dbPath);
 }
+
 
 /* Mother */
 
 async function initialBreadcrumbs() {
   let r = await invoke("suggest_path", {});
-  let datapasePath = r[1];
-  InformStatus(JSON.stringify(datapasePath));
-  updateDatabasePath(datapasePath);
+  let dbPath = r[1];
+  updateDatabasePath(dbPath);
+  updateBreadcrumbs(dbPath);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
